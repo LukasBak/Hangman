bacad832d5c75fd5c181c64c69baf3df6224364b
Lukas Bakanauskas <bleizius@gmail.com>
2024-02-28 15:28:57 +0000

    Initial commit, added battles.py

---
+++ battles.py
@@ -0,0 +1,18 @@
+""
+Famous medieval battles across europe used inside the game
+""
+
+battle_word {
+    'Grundwald',
+    'Hastings',
+    'Antioch',
+    'Bannockburn',
+    'Mohi',
+    'Castillon',
+    'Bannockburn',
+    'Constantinople',
+    'Grenada',
+    'Saule'
+
+
+}
--- run.py
+++ run.py
@@ -1,3 +1,6 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import battles.py
+import random
+
+random_word = battles.py
+
+print(random_word)
