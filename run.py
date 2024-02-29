7789b09ffb273391e60f2ba999ef487d1b24a00f
Lukas Bakanauskas <bleizius@gmail.com>
2024-02-29 17:07:41 +0000

    Removed py file, added main function, random choice

--- run.py
+++ run.py
@@ -1,6 +1,25 @@
-import battles.py
 import random
 
-random_word = battles.py
 
-print(random_word)
+
+
+battles = [
+    "Grundwald",
+    "Hastings",
+    "Antioch",
+    "Bannockburn",
+    "Mohi",
+    "Constantinople",
+    "Grenada",
+    "Saule"
+]
+
+
+def choose_random_battle():
+    return random.choice(battles)
+
+def main():
+    word = choose_random_battle()
+    print(word)
+
+main()
