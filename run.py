a286113a4f25fd413610120d96e5d2260d6d5150
Lukas Bakanauskas <bleizius@gmail.com>
2024-03-01 10:14:03 +0000

    Added rules section

--- run.py
+++ run.py
@@ -1,8 +1,6 @@
 import random
 
 
-
-
 battles = [
     "Grundwald",
     "Hastings",
@@ -14,12 +12,29 @@ battles = [
     "Saule"
 ]
 
+player_name = input("Enter your name or nickname " )
+print(f"Welcome {player_name} to medieval battles hangman!")
+
+def print_rules():
+    """
+    Rules and tips for the game
+    """
+    print("Rules and tips for medieval battles hangman are as follow: ")
+    print("1. Battles chosen are in medieval era.")
+    print("2. Battles are chosen are based in europe and are mostly city or town names")
+    print("3. Letters that has been correctly selected will apear on the word")
+    print("4. Incorrect guesses will reduce your remaning attempts by one until it hits 0")
+    print("5. If you run out of attempts the game will be over.")
 
 def choose_random_battle():
+    """
+    Function for random battle choice.
+    """
     return random.choice(battles)
 
 def main():
+    rules = print_rules()
     word = choose_random_battle()
-    print(word)
+    
 
 main()
