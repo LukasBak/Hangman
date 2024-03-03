ecc18d2e7142198810021ff0702c9b0d2d421189
Lukas Bakanauskas <bleizius@gmail.com>
2024-03-03 06:20:34 +0000

    Added some extra battles, shifted prints for more fluidity

--- run.py
+++ run.py
@@ -10,11 +10,17 @@ battles = [
     "Constantinople",
     "Grenada",
     "Saule"
+    "Agincourt"
+    "Bouvines"
+    "Falkirk"
+    "Hattin"
+    "Carham"
+    "Tours"
+    "Oribetello"
+    "Clontarf"
+    "Bordeaux"
 ]
 
-
-
-    
 def print_rules():
     """
     Rules and tips for the game
@@ -23,8 +29,10 @@ def print_rules():
     print("1. Battles chosen are in medieval era.\n")
     print("2. Battles are chosen are based in europe and are mostly city or town names.\n")
     print("3. Letters that has been correctly selected will apear on the word.\n")
-    print("4. Incorrect guesses will reduce your remaning attempts by one until it hits 0.\m")
+    print("4. Incorrect guesses will reduce your remaning attempts by 1 until it hits 0.\n")
     print("5. If you run out of attempts the game will be over.\n ")
+    print("6. If name not provided it be left as blank.")
+
 
 def choose_random_battle(battles):
     return random.choice(battles).upper()
@@ -47,14 +55,20 @@ def display_word(word, guessed_letters):
 
 
 def get_user_guess(battles):
+    """
+    With every guess a Letter either remains hidden and player reduces amount of 
+    attempts has, if guessed correctly remain same.
+    Second loop of the game, does not finish until everything is guessed or ran out of attempts.
+    """
     guessed_letters = []
     attempts = 6
     choose_battle = choose_random_battle(battles)
 
     while attempts > 0:
-        guess = input("Your guess:\n ").upper()
         print("Your word:\n", display_word(choose_battle, guessed_letters))
-        print("Your attempts:", attempts)
+        print("Remanining attempts:\n", attempts)
+        guess = input("Your guess:\n ").upper()
+        
         
         if guess in guessed_letters:
             print("You already guessed this!\n")
@@ -75,18 +89,21 @@ def get_user_guess(battles):
             break
 
 
-
-
 def main():
+    """
+    First loop when enterering the game.
+    """
     player_name = input("Enter your name or nickname \n" )
     print(f"Welcome {player_name} to medieval battles hangman!") 
+    
     print_rules()
     choose_battle = choose_random_battle(battles)
     guessed_letters = []
+    
     while True:
         play_game = input(f"Would you like to continue? y or n  \n")
         if play_game.lower() == "y":
-            input(f"Are you ready to continue? y or n \n")
+            input(f"If you are ready press enter: \n")
             get_user_guess(battles)
             break
         elif play_game.lower() == "n":
