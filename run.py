7dd4c3c287e32d367fb9b12e4edd3d667e673c01
Lukas Bakanauskas <bleizius@gmail.com>
2024-03-03 06:44:29 +0000

    Fixed the rule section, reduced unuses space.

--- run.py
+++ run.py
@@ -21,17 +21,19 @@ battles = [
     "Bordeaux"
 ]
 
+
 def print_rules():
     """
     Rules and tips for the game
     """
     print("Rules and tips for medieval battles hangman are as follow:\n ")
     print("1. Battles chosen are in medieval era.\n")
-    print("2. Battles are chosen are based in europe and are mostly city or town names.\n")
-    print("3. Letters that has been correctly selected will apear on the word.\n")
-    print("4. Incorrect guesses will reduce your remaning attempts by 1 until it hits 0.\n")
-    print("5. If you run out of attempts the game will be over.\n ")
-    print("6. If name not provided it be left as blank.")
+    print("2. Battles are chosen and based in europe and named after areas.\n")
+    print("3. If letter guessed right it will apear on board.\n")
+    print("4. Wrong guess will reduce your remaning attempts by 1.\n")
+    print("5. If you attempts remaining become 0 its game over.\n")
+    print("6. If you run out of attempts the game will be over.\n ")
+    print("7. If name not provided it be left as blank.\n")
 
 
 def choose_random_battle(battles):
@@ -43,7 +45,7 @@ def choose_random_battle(battles):
 
 def display_word(word, guessed_letters):
     """
-    Function for displaying guessed letters / unguessed letters appear as underscores
+    Function for playing the game
     """
     display = []
     for letter in word:
@@ -54,12 +56,14 @@ def display_word(word, guessed_letters):
     return "".join(display)
 
 
+"""
+With every guess a Letter either remains hidden and player reduces amount of
+attempts has, if guessed correctly remain same.
+Second loop of the where most logic is stored.
+    """
 def get_user_guess(battles):
-    """
-    With every guess a Letter either remains hidden and player reduces amount of 
-    attempts has, if guessed correctly remain same.
-    Second loop of the game, does not finish until everything is guessed or ran out of attempts.
-    """
+
+
     guessed_letters = []
     attempts = 6
     choose_battle = choose_random_battle(battles)
@@ -68,8 +72,6 @@ def get_user_guess(battles):
         print("Your word:\n", display_word(choose_battle, guessed_letters))
         print("Remanining attempts:\n", attempts)
         guess = input("Your guess:\n ").upper()
-        
-        
         if guess in guessed_letters:
             print("You already guessed this!\n")
             continue
@@ -80,12 +82,11 @@ def get_user_guess(battles):
             attempts -= 1
         else:
             print("Well done! You guessed a correct letter!\n")
-
         if display_word(choose_battle, guessed_letters) == choose_battle:
             print("Congratulations! You guessed the word:\n", choose_battle)
             break
         elif attempts == 0:
-            print("You have run out of attempts! The word was:\n", choose_battle)
+            print("You've run out of attempts! The word was:\n", choose_battle)
             break
 
 
@@ -93,13 +94,11 @@ def main():
     """
     First loop when enterering the game.
     """
-    player_name = input("Enter your name or nickname \n" )
-    print(f"Welcome {player_name} to medieval battles hangman!") 
-    
+    player_name = input("Enter your name or nickname \n")
+    print(f"Welcome {player_name} to medieval battles hangman!")
     print_rules()
     choose_battle = choose_random_battle(battles)
     guessed_letters = []
-    
     while True:
         play_game = input(f"Would you like to continue? y or n  \n")
         if play_game.lower() == "y":
