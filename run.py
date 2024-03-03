b32194932b0f5bef5d5fad04c207f5e2fe13e2aa
Lukas Bakanauskas <bleizius@gmail.com>
2024-03-03 05:40:13 +0000

    Solved display word and get user guess function

--- run.py
+++ run.py
@@ -20,80 +20,79 @@ def print_rules():
     Rules and tips for the game
     """
     print("Rules and tips for medieval battles hangman are as follow:\n ")
-    print("1. Battles chosen are in medieval era.")
-    print("2. Battles are chosen are based in europe and are mostly city or town names")
-    print("3. Letters that has been correctly selected will apear on the word")
-    print("4. Incorrect guesses will reduce your remaning attempts by one until it hits 0")
+    print("1. Battles chosen are in medieval era.\n")
+    print("2. Battles are chosen are based in europe and are mostly city or town names.\n")
+    print("3. Letters that has been correctly selected will apear on the word.\n")
+    print("4. Incorrect guesses will reduce your remaning attempts by one until it hits 0.\m")
     print("5. If you run out of attempts the game will be over.\n ")
 
-
-def display_word(word, guessed_letters):
-    displayed_word = " "
-    for letter in word:
-        if letter in guessed_letters:
-            displayed_word += letter + ""
-        else: 
-                displayed_word += ""
-                return displayed_word.strip()
-
-
-
-def get_user_guess(display_word,):
-    while True:
-
-        guess = input("Your guess:  ").upper()
-        if guess in guessed_letters:
-            print("You already tried this!")
-        elif guess in display_word:
-           print("You are correct!")
-           display += guess + " "
-        else:
-            attempts -= 1
-            print("Game over! You have been hanged!")
-
-
-"""
-def guessed_letters(display_word,):
-    if guess is display_word:
-        print("You are correct!") 
-"""
-
-
-
-def choose_random_battle():
+def choose_random_battle(battles):
+    return random.choice(battles).upper()
     """
     Function for random battle choice.
     """
-    return random.choice(battles).upper()
+
+
+def display_word(word, guessed_letters):
+    """
+    Function for displaying guessed letters / unguessed letters appear as underscores
+    """
+    display = []
+    for letter in word:
+        if letter in guessed_letters:
+            display.append(letter)
+        else:
+            display.append("_")
+    return "".join(display)
+
+
+def get_user_guess(battles):
+    guessed_letters = []
+    attempts = 6
+    choose_battle = choose_random_battle(battles)
+
+    while attempts > 0:
+        guess = input("Your guess:\n ").upper()
+        print("Your word:\n", display_word(choose_battle, guessed_letters))
+        print("Your attempts:", attempts)
+        
+        if guess in guessed_letters:
+            print("You already guessed this!\n")
+            continue
+
+        guessed_letters.append(guess)
+        if guess not in choose_battle:
+            print("You are incorrect!\n")
+            attempts -= 1
+        else:
+            print("Well done! You guessed a correct letter!\n")
+
+        if display_word(choose_battle, guessed_letters) == choose_battle:
+            print("Congratulations! You guessed the word:\n", choose_battle)
+            break
+        elif attempts == 0:
+            print("You have run out of attempts! The word was:\n", choose_battle)
+            break
+
+
+
 
 def main():
     player_name = input("Enter your name or nickname \n" )
     print(f"Welcome {player_name} to medieval battles hangman!") 
     print_rules()
-    word = choose_random_battle()
-    display_word = "_" * len(word)
-    get_user_guess(display_word)
-    attempts = 6
+    choose_battle = choose_random_battle(battles)
+    guessed_letters = []
     while True:
         play_game = input(f"Would you like to continue? y or n  \n")
         if play_game.lower() == "y":
-            print(f"This is your word: ", display_word )
+            input(f"Are you ready to continue? y or n \n")
+            get_user_guess(battles)
             break
         elif play_game.lower() == "n":
             print(f"{player_name} chose not to play! Returning to start")
             main()
             exit()
-    print("Code after loop")
-    """
-    get_user_guess()
-    """
-    
-   
-    """
-    display_word = word + "_"  len(word)
- """
-"""
-display_word(word,guessed_letters):
-"""
+
 
 main()
